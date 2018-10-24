7f923d641947be267969ca65febaf40f4f1ae4ff
root <root@d9.hph.io>
2018-10-24 11:52:01 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # sysutils
 A series of scripts meant to make working with servers easier.
+
+# Installation
+
+To installt these scripts, clone them to some place reasonable (/usr/share?), and then run `install.sh` as root. To uninstall, run `uninstall.sh` as root.
+
+# Utilities
+
+## reset-permissions
+
+Resets permissions to a specified path to the default Linux permissions and changes ownership to the owner specified. By default, it will not make any changes to the system, You must specify the `--commit` argument in order to make the changes.
+
+Example:
+
+`reset-permissions /home/foo baruser --commit`
---
+++ reset-permissions.sh
@@ -0,0 +1,115 @@
+#!/usr/bin/env bash
+
+check_root() {
+    if [ $(whoami) != "root" ]; then
+      echo "$0 should be run as root! You're not root. Magic 8 ball says: RTFM."
+      usage
+      exit 1
+    fi
+}
+
+usage() {
+    cat <<EOF
+
+Reset Permissions
+
+Sets default system permissions for a specified path, and fixes ownership.
+
+Syntax: reset-permissions path [user] [commit]
+
+Where:
+    path is the path you want to reset permissions.
+    user is the user who should own the path
+    commit tells the system you really do want to commit your changes ot the file system.
+
+    If no user is specified, the current user is assumed.
+
+Examples:
+
+    To change all the files in /home/foo/:
+
+        reset-permissions /home/foo myuser --commit
+
+
+Support and issues should be filed on github: https://github.com/mjmunger/sysutils
+EOF
+}
+
+check_args() {
+    if [ $1 -lt 1 ];
+    then
+        usage
+        exit 1
+    fi
+}
+
+error_out() {
+    echo ""
+    echo "ERROR: " $1
+    echo ""
+    exit 1
+}
+
+check_path() {
+    if ! [ -d $1 ];
+    then
+        error_out "Path $1 does not exist."
+        usage
+    fi
+}
+
+help_hint() {
+    echo ""
+    echo 'type `./reset-permissions help` for command syntax and usage.'
+    echo ""
+}
+check_user() {
+    EXISTS=$(cat /etc/shadow | grep $1 | cut -d ':' -f1)
+    if [ -z ${EXISTS} ]; then
+        echo ""
+        echo "ERROR: The specified user $1 does not exist!";
+        help_hint
+        exit 1
+    fi
+}
+
+set_file_permissions() {
+    echo $([[ "$3" == '--dryrun' ]]) && echo "DRY RUN!" || echo "Commiting changes to file system."
+
+    echo "Setting ownership for $1 to $2:$2..."
+
+    if [[ "$3" != "--commit" ]]; then
+        return 0
+    fi
+
+    cd $1
+    echo -n "Setting file ownership..."
+    find . -type f -exec chown -R $2:$2 {} \; >/dev/null 2>&1
+    echo "OK"
+    echo -n "Setting directory ownership..."
+    find . -type d -exec chown -R $2:$2 {} \; >/dev/null 2>&1
+    echo "OK"
+
+    echo -n "Setting file permissions..."
+    find . -type f -exec mode 0644 {} \; >/dev/null 2>&1
+    echo "OK"
+
+    echo -n "Setting directory permissions..."
+    find . -type d -exec mode 0755 {} \; >/dev/null 2>&1
+    echo "OK"
+
+}
+
+check_root
+check_args $#
+check_path $1
+
+TARGETPATH=$( [ "$1" == '.' ] && echo `pwd` || echo $1 )
+
+USER=$(echo `whoami`)
+if [ $# -gt 2 ]; then
+    check_user $2
+    USER=$2
+fi
+COMMIT=$( [[ $# -eq 3 ]] && [[ "$3" == '--commit' ]] && echo '--commit' || echo '--dryrun' )
+set_file_permissions ${TARGETPATH} ${USER} ${COMMIT}
