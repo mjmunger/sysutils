747033b48f2028867b94cadd00f1952ecb453660
michael <mj@hph.io>
2018-10-30 15:22:16 -0400

    Fixed an if statement

--- scripts/install-python-36.sh
+++ scripts/install-python-36.sh
@@ -2,7 +2,7 @@
 install_python_36() {
     BUFFER=$(cat /etc/apt/sources.list | grep 'deb http://ftp.de.debian.org/debian testing main')
 
-    if [ -z ${BUFFER} ]; then
+    if [[ -z ${BUFFER} ]]; then
         printf "\ndeb http://ftp.de.debian.org/debian testing main\n" >> /etc/apt/sources.list
         echo 'APT::Default-Release "stable";' | sudo tee -a /etc/apt/apt.conf.d/00local
     fi
