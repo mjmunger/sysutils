67a380e741aad6ec81202811ee2f507872bcbb3d
michael <mj@hph.io>
2018-10-30 15:21:05 -0400

    Fixed an if statement

--- scripts/install-python-36.sh
+++ scripts/install-python-36.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 install_python_36() {
     BUFFER=$(cat /etc/apt/sources.list | grep 'deb http://ftp.de.debian.org/debian testing main')
-    [ -z ${BUFFER} ] && printf "\ndeb http://ftp.de.debian.org/debian testing main\n" >> /etc/apt/sources.list
-    echo 'APT::Default-Release "stable";' | sudo tee -a /etc/apt/apt.conf.d/00local
+
+    if [ -z ${BUFFER} ]; then
+        printf "\ndeb http://ftp.de.debian.org/debian testing main\n" >> /etc/apt/sources.list
+        echo 'APT::Default-Release "stable";' | sudo tee -a /etc/apt/apt.conf.d/00local
+    fi
+
     apt-get update
     apt-get -t testing install python3.6
 }
