b631b7d4ecf10a55c863caaec861dcc5241730be
michael <mj@hph.io>
2019-04-12 12:55:08 -0400

    Added / refactored package installer to use self-contained scripts with names and summaries int header.

--- scripts/install_package.sh
+++ scripts/install_package.sh
@@ -62,9 +62,6 @@ EOF
 install_package() {
 
     PACKAGE=$1
-    echo "Package=${PACKAGE}"
-    echo "Package Install directory: ${PACKAGEINSTALLDIR}"
-
     INSTALLSCRIPT=${PACKAGEINSTALLDIR}/${PACKAGE}.sh
 
     if [ -z ${PACKAGE} ]; then
@@ -77,28 +74,6 @@ install_package() {
         exit 1
     fi
 
-
-    [ -z $1 ] && error_out "You must specify a package to install"
-
-    case $1 in
-        'python3-deb')
-            source ${SCRIPTSDIR}/install-python-36.sh
-            install_python_36
-            ;;
-        'pyrpg')
-            install_rpg
-            ;;
-        'composer')
-            install_composer
-            ;;
-        'bacula-dir')
-            install_bacula
-            ;;
-        'bacula-fd')
-            install_bacula_fd
-            ;;
-        *)
-            error_out "Package $1 is not found."
-            ;;
-    esac
+    source ${INSTALLSCRIPT}
+    run_installer
 }
