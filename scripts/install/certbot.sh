0cb4017e22d90d92cbdb929be92847ffc7622c20
michael <mj@hph.io>
2019-04-12 14:12:31 -0400

    Added certbot

---
+++ scripts/install/certbot.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# certbot
+# Install certbot for Debian 9 (stretch).
+
+run_installer() {
+    CERBOTLIST=/etc/apt/sources.list.d/certbot
+
+    if [ ! -f ${CERBOTLIST} ]; then
+        printf "\ndeb http://deb.debian.org/debian stretch-backports main" > ${CERBOTLIST}
+    fi
+
+    apt -y update
+    apt -y upgrade
+    apt -y install certbot python-certbot-apache -t stretch-backports
+}
